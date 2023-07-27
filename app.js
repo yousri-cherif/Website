ea78971ec769d8ee3f0e8297de0b92f0258e1794
Yousri Cherif <>
2023-07-28 01:54:35 +0200

    Security fix (innerhtml deleted)

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-try {
-  var date = new Date();
-  var footer_text =
-    "© " + date.getFullYear() + " Yousri cherif. All rights reserved.";
-  document.getElementById("copyright_text").innerHTML = footer_text;
-} catch (error) {
-  console.error("Error, enable to get date", error);
-  document.getElementById("copyright_text").innerHTML =
-    "© Yousri cherif. All rights reserved.";
-}
-
-if (window.location.href.includes("index.html")) {
+let currentPage = document.body.id;
+if (currentPage == "index") {
   document.querySelectorAll(".image_container img").forEach((image) => {
     image.onclick = () => {
       document.querySelector(".popup_image").style.display = "block";
@@ -20,4 +10,15 @@ if (window.location.href.includes("index.html")) {
   document.querySelector(".popup_image span").onclick = () => {
     document.querySelector(".popup_image").style.display = "none";
   };
+}
+
+try {
+  var date = new Date();
+  var footer_text =
+    "© " + date.getFullYear() + " Yousri cherif. All rights reserved.";
+  document.getElementById("copyright_text").textContent = footer_text;
+} catch (error) {
+  console.error("Error, enable to get date", error);
+  document.getElementById("copyright_text").innerHTML =
+    "© Yousri cherif. All rights reserved.";
 }
