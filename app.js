9f038af0f25b255faf52f8656f65b2cf59d015a0
Yousri Cherif <>
2023-11-25 01:20:28 +0100

    Image refresh fix

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ function setFooterText() {
 function addPopupImageBehavior() {
   document.querySelectorAll(".image_container img").forEach((image) => {
     image.onclick = () => {
-      document.querySelector(".popup_image").style.display = "block";
       document.querySelector(".popup_image img").src =
         image.getAttribute("src");
+      document.querySelector(".popup_image").style.display = "block";
     };
   });
   document.querySelector(".popup_image span").onclick = () => {
