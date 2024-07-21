c3e978914e08c0d8445dd37ffae2a0f49cb4a462
Yousri Cherif <>
2024-07-21 16:44:48 +0200

    Left right navigation with keyboard

--- app.js
+++ app.js
@@ -1,44 +1,59 @@
-function getCurrentYear() {
-  try {
-    return new Date().getFullYear().toString();
-  } catch (error) {
-    console.error("Error while getting the current year:", error);
-    return ""; // Fallback to empty string if an error occurs
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".image_container .image");
+  const popup = document.querySelector(".popup_image");
+  const popupImg = popup.querySelector("img");
+  const closeButton = popup.querySelector("span");
+
+  let currentIndex = -1;
+
+  function showPopup(index) {
+    currentIndex = index;
+    const imageSrc = images[currentIndex].src;
+    const imageAlt = images[currentIndex].alt;
+    popupImg.src = imageSrc;
+    popupImg.alt = imageAlt;
+    popup.style.display = "flex";
   }
-}
 
-function setFooterText() {
-  const footerElement = document.getElementById("copyright_text");
-  const currentYear = getCurrentYear();
-  const footerText = `© ${currentYear} Yousri Cherif. All rights reserved.`;
-  footerElement.textContent = footerText;
-}
+  function closePopup() {
+    popup.style.display = "none";
+    currentIndex = -1;
+  }
 
-function addPopupImageBehavior() {
-  document.querySelectorAll(".image_container img").forEach((image) => {
-    image.onclick = () => {
-      document.querySelector(".popup_image img").src =
-        image.getAttribute("src");
-      document.querySelector(".popup_image").style.display = "block";
-    };
+  function showNextImage() {
+    if (currentIndex < images.length - 1) {
+      showPopup(currentIndex + 1);
+    }
+  }
+
+  function showPreviousImage() {
+    if (currentIndex > 0) {
+      showPopup(currentIndex - 1);
+    }
+  }
+
+  images.forEach((image, index) => {
+    image.addEventListener("click", () => showPopup(index));
   });
-  document.querySelector(".popup_image span").onclick = () => {
-    document.querySelector(".popup_image").style.display = "none";
-  };
-  document.addEventListener("keydown", function (event) {
-    if (event.key === "Escape") {
-      document.querySelector(".popup_image").style.display = "none";
+
+  closeButton.addEventListener("click", closePopup);
+
+  document.addEventListener("keydown", (e) => {
+    if (popup.style.display === "flex") {
+      if (e.key === "ArrowRight") {
+        showNextImage();
+      } else if (e.key === "ArrowLeft") {
+        showPreviousImage();
+      } else if (e.key === "Escape") {
+        closePopup();
+      }
     }
   });
-}
 
-document.addEventListener("DOMContentLoaded", () => {
-  setFooterText();
-
-  const currentPage = document.body.id;
-  if (currentPage === "index") {
-    addPopupImageBehavior();
-  }
+  // Set current year in the footer
+  document.getElementById(
+    "copyright_text"
+  ).textContent = `© ${new Date().getFullYear()} Yousri Cherif. All rights reserved.`;
 });
 
 function transformScroll(event) {
